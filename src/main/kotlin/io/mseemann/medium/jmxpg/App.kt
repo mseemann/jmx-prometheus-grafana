93e396e95458a497dc7ceaab6db491b9ba30e132
Michael Seemann <michael.seemann@synaos.com>
2019-12-28 19:01:51 +0100

    more concise

--- src/main/kotlin/io/mseemann/medium/jmxpg/App.kt
+++ src/main/kotlin/io/mseemann/medium/jmxpg/App.kt
@@ -3,10 +3,7 @@ package io.mseemann.medium.jmxpg
 import io.mseemann.medium.jmxpg.beans.AppInfo
 import io.mseemann.medium.jmxpg.beans.Requests
 import java.lang.management.ManagementFactory
-import java.util.*
 import javax.management.ObjectName
-
-object App
 
 const val NAMESPACE = "jmxpg.mbeans"
 const val GET_REQUESTS_O_NAME = "$NAMESPACE:type=Requests,verb=GET"
@@ -22,10 +19,8 @@ fun main() {
 
     ManagementFactory.getPlatformMBeanServer().apply {
 
-        val keyValues = Hashtable<String, String>()
-        keyValues["type"] = "AppInfo"
-        keyValues["Version"] = App::class.java.getPackage().implementationVersion ?: "n.n."
-        registerMBean(appInfo, ObjectName(NAMESPACE, keyValues))
+        val version = AppInfo::class.java.getPackage().implementationVersion ?: "n.n."
+        registerMBean(appInfo, ObjectName("$NAMESPACE:type=AppInfo,version=$version"))
 
         registerMBean(getRequests, ObjectName(GET_REQUESTS_O_NAME))
         registerMBean(postRequests, ObjectName(POST_REQUESTS_O_NAME))
